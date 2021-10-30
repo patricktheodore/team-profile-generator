fb83609f01dd77ffc08328ac85e806f8571e563c
patricktheodore <patricktheodoresara@gmail.com>
2021-10-30 23:09:47 +0800

    app.js inquirer functionality

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const questions = require('./utils/questions');
+const renderTeam = require('./utils/renderTeam')
 
 const fs = require('fs');
 const inquirer = require('inquirer');
@@ -14,84 +15,55 @@ const employees = [];
 //add employee data to employees array. 
 
 //see if user wants to add another employee
-
-let employee = {} 
-
-return inquirer.prompt(questions.managerQuestion).then((answer) => {
-    employee['role'] = 'Manager';
-    employee['name'] = answer.name;
-    employee['id'] = answer.id;
-    employee['email'] = answer.email;
-    employee['officeNumber'] = answer.officeNumber;
-
-    const addEmployee = new Manager (employee['name'], employee['id'], employee['email'], employee['officeNumber']);
-
+function managerQuestions() {
+ return inquirer.prompt(questions.managerQuestion).then((answer) => {
+    const addEmployee = new Manager (answer.name, answer.id, answer.email, answer.officeNumber);
     employees.push(addEmployee);
-
-    console.log(employees)
-
-})
-
-
-
-
-
+    addAnotherEmployee();
+});
+}
 
 function addAnotherEmployee() {
-    return inquirer.prompt(questions.confirmNewEmployee)
+    return inquirer.prompt(questions.confirmNewEmployee).then((answer) => {
+        if (answer.newEmployee) {
+            promptEmployeeType();
+        } else {
+            //render html - todo 
+            renderTeam(employees);
+        }
+    })
 }
 
 function promptEmployeeType() {
-    return inquirer.prompt(questions.confirmNewEmployeeType)
+    return inquirer.prompt(questions.confirmNewEmployeeType).then((answer) => {
+        if (answer.role === 'manager') {
+            managerQuestions();
+        } else if (answer.role === 'engineer') {
+            engineerQuestions();
+        } else {
+            internQuestions();
+        }
+    });
 }
 
+function engineerQuestions() {
+    return inquirer.prompt(questions.engineerQuestions).then((answer) => {
+       const addEmployee = new Engineer (answer.name, answer.id, answer.email, answer.github);
+       employees.push(addEmployee);
+       addAnotherEmployee();
+   });
+   }
 
+   function internQuestions() {
+    return inquirer.prompt(questions.internQuestions).then((answer) => {
+       const addEmployee = new Intern (answer.name, answer.id, answer.email, answer.school);
+       employees.push(addEmployee);
+       addAnotherEmployee();
+   });
+   }
 
-function managerQuestion() {
-    inquirer.prompt(questions.managerQuestion)
+function init() {
+    managerQuestions();
 }
 
-inquirer.prompt(questions.confirmNewEmployee)
-.then((data) => {
-    console.log(data);
-    if (data.newEmployee) {
-
-    }
-});
-
-// if(role.toLowerCase() === 'manager') {
-//     return inquirer.prompt(questions.managerQuestion)
-// }
-
-// if(role.toLowerCase() === 'engineer') {
-//     return inquirer.prompt(questions.engineerQuestions)
-// }
-
-// if(role.toLowerCase() === 'intern') {
-//     return inquirer.prompt(questions.internQuestions)
-// }
-
-// geneerateTeamProfile();
-
-
-// user is prompted for the manager
-
-//who else is in the team
-//add intern or engineer
-
-// until, no one else
-//generate html.
-
-//logic
-
-//ask manager question, confirm if they want to add another manager. 
-//if yes, run managerquestion prompt
-
-//if no, ask which employee would you like to add? 
-//check if intern or engineer, ask appropriate questions. 
-//ask if they would like to add another employee. 
-//if yes , ask which employee, and run apparopraite questions
-//if no, run geneerateTeamProfile function. 
-
-
-//store employee data
+init();
--- utils/questions.js
+++ utils/questions.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 function commonQuestions(role="manager"){
     return [
         {
